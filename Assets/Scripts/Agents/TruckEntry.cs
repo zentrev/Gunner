3784e9e35071e7fea8c85fa3765272c9ca0961b4
Jonathon Lau <Jonathon_lau@hotmail.com>
2019-10-05 20:33:31 -0600

    ah

---
+++ Assets/Scripts/Agents/TruckEntry.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckEntry : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.parent = null;
+        gameObject.GetComponent<TruckFlock>().enabled = false;
+        if (LevelCreator.Instance.Enemies.Count >= 5)
+        {
+            print("bleh");
+            Destroy(gameObject);
+        }
+        else
+        {
+            LevelCreator.Instance.Enemies.Add(gameObject);
+            print("yay");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(Vector3.Distance(gameObject.transform.position, Camera.main.transform.position));
+        if (Vector3.Distance(gameObject.transform.position, Camera.main.transform.position) < 500)
+        {
+            EnterCanyon();
+            enabled = false;
+        }
+    }
+
+    void EnterCanyon()
+    {
+        print("vroom vroom x" + LevelCreator.Instance.Enemies.Count);
+    }
+}
