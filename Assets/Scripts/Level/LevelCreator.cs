1b789aa518238316e83e94e58ec695831f028640
Kaedian <38064751+Kaedian-Maxfield@users.noreply.github.com>
2019-10-05 20:26:57 -0600

    dik

--- Assets/Scripts/Level/LevelCreator.cs
+++ Assets/Scripts/Level/LevelCreator.cs
@@ -7,12 +7,14 @@ public class LevelCreator : Singleton<LevelCreator>
     [SerializeField] GameObject[] canyonBits;
 
     public GameObject[] CanyonBits { get => canyonBits; set => canyonBits = value; }
+    public List<GameObject> Enemies { get => enemies; set => enemies = value; }
 
+    private List<GameObject> enemies = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemies = new List<GameObject>();
     }
 
     // Update is called once per frame
